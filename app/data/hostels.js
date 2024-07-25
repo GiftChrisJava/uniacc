c6c0cf188db0e39b5441453cdd29cef34e2de4fb
GiftChrisJava <chrisjava77@gmail.com>
2024-07-25 11:28:18 +0200

    images added (dummy)

---
+++ app/data/hostels.js
@@ -0,0 +1,24 @@
+import img1 from "../../public/a.jpeg"
+import img2 from "../../public/b.jpeg"
+import img3 from "../../public/c.jpeg"
+import img5 from "../../public/2.jpeg"
+import img6 from "../../public/3.jpeg"
+
+export const hostelsData = [
+    {
+      id: 1,
+      name: 'Hostel 1',
+      images: [img1, img2, img3],
+      owner: 'John Doe',
+      location: 'On-campus'
+    },
+    {
+      id: 2,
+      name: 'Hostel 2',
+      images: [img5, img6],
+      owner: 'Jane Smith',
+      location: 'Off-campus'
+    },
+    // Add more hostels here
+  ];
+  
